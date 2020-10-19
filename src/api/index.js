3ae784dfc2b421f45d6054ca83156ded5076e071
Lionel Francis <lezardvaldth@gmail.com>
2020-10-19 12:31:09 -0500

    updates

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import Vue from 'vue';
 export default() => {
     return axios.create({
         baseURL: 'https://majesticaring.com/api/v1/',
+
         // baseURL: 'http://127.0.0.1:8003/api/v1/',
         withCredentials: false,
         headers: {
