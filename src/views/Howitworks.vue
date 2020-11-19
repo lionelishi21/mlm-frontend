04b9aeb87bef083bce0cc19a429cb7260fd2ed9a
Lionel Francis <lezardvaldth@gmail.com>
2020-11-19 10:06:48 -0500

    update how it works at last

--- src/views/Howitworks.vue
+++ src/views/Howitworks.vue
@@ -1,164 +1,305 @@
 <template>
-<!-- 	<section class="page-header page-header-classic page-header-md">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-8 order-2 order-md-1 align-self-center p-static">
-					<h1 data-title-border>Services</h1>
+
+	<div role="main" class="main">
+		<section class="page-header page-header-classic page-header-md">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+						<h1 data-title-border>Services</h1>
+					</div>
+					<div class="col-md-4 order-1 order-md-2 align-self-center">
+						<ul class="breadcrumb d-block text-md-right">
+							<li><a href="#">Home</a></li>
+							<li class="active">Pages</li>
+						</ul>
+					</div>
 				</div>
-				<div class="col-md-4 order-1 order-md-2 align-self-center">
-					<ul class="breadcrumb d-block text-md-right">
-						<li><a href="#">Home</a></li>
-						<li class="active">Pages</li>
-					</ul>
+			</div>
+		</section>
+
+		<div class="container pb-1">
+
+			<div class="row pt-4">
+				<div class="col">
+					<div class="overflow-hidden mb-3">
+						<h2 class="font-weight-bold text-8 mb-0 appear-animation" data-appear-animation="maskUp">
+							Welcome to The Majestic Cares Community!  “MCC”
+						</h2>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="overflow-hidden">
+						<p class="lead mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200">
+							is an online community inspired by Ordinary People Worldwide.
+							It was created so that ALL of its Affiliate/Members can receive a residual income for any worthwhile purpose, such as:
+							business start-up, paying off debts, buying a new car or home, or travelling abroad to any desired destination!
+						</p>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="row pt-4">
+				<div class="col-lg-12">
+					<div class="overflow-hidden">
+						<p class="lead mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200">
+							No Matter what your current financial needs are, The Majestic Cares Online Community (MCC),
+							is the answer to helping you and your loved ones, to fulfill your financial hopes and dreams!
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row pt-4">
+				<div class="col-lg-12">
+					<div class="overflow-hidden">
+						<p class="lead mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200">
+							Please First Note, that this is NOT an MLM, Net-Work-Marketing, or a Get-Rich-Quick Community. The MCC Community gives each Affiliate/Member,
+							the unique opportunity of receiving Community Shared Cash Bonuses based on the Affiliate’s Group Sales Volume.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row pt-4">
+				<div class="col-lg-12">
+					<div class="overflow-hidden">
+						<p class="lead mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200">
+							There are no Enrollment or Administration Fees. There is No Recruiting or Sponsoring Required to Become an MCC Sales Affiliate.
+							However, in order to start receiving Community Shared Cash Bonuses, an Affiliate must make no less than 3 personal sales of MCC’S Self-Help Publication entitled,
+							“Learn How to Stop Working for Money, and Let Your Money Work for You” which sells for $34.95, and comes with a 30-day Money Back Guarantee.
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col py-3">
+					<hr class="solid mt-5 mb-2">
+				</div>
+			</div>
+
+		</div>
+
+		<section class="section bg-color-grey section-height-3 border-0 mt-5 mb-0">
+			<div class="container">
+				<div class="row pt-4">
+					<div class="col">
+						<div class="overflow-hidden mb-3">
+							<h2 class="font-weight-bold text-8 mb-0 appear-animation" data-appear-animation="maskUp">
+								Here Is an Example of How This Works:
+							</h2>
+							<p class="lead mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200">
+								When an MCC Sales Affiliate makes a minimum of 3 Personal Sales,
+								the Affiliate qualifies to receive Community Shared Cash Bonuses and is positioned within an MCC Group of 12 other qualifiers who begin to receive
+								Community Shared Cash Bonuses as per MCC’S Automated Compensation Plan.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col">
+						<div class="row align-items-center pt-5 pb-3 appear-animation" data-appear-animation="fadeInRightShorter">
+							<div class="col-md-12 pr-md-5 mb-5 mb-md-0">
+								<h2 class="font-weight-normal text-6 mb-1"><strong class="font-weight-extra-bold">BRONZE</strong> Affiliate:</h2>
+								<p class="text-4">
+									When your personal group has a total of 12 Group eBook sales,
+									you will receive a Cash Bonus of $100
+								</p>
+							</div>
+						</div>
+
+						<div class="row align-items-center appear-animation" data-appear-animation="fadeInRightShorter">
+							<div class="col-md-12">
+								<h2 class="font-weight-normal text-6"><strong class="font-weight-extra-bold">And Then Advance To:</strong></h2>
+							</div>
+						</div>
+
+						<div class="row align-items-center pt-3 pb-3 appear-animation" data-appear-animation="fadeInRightShorter">
+							<div class="col-md-12 pr-md-5 mb-5 mb-md-0">
+								<h2 class="font-weight-normal text-6 mb-1"><strong class="font-weight-extra-bold">SILVER</strong> Affiliate:</h2>
+								<p class="text-4">
+									When your personal group has a total of 36 Group eBook sales,
+									you will receive a Cash Bonus of $600
+								</p>
+							</div>
+						</div>
+
+						<div class="row align-items-center appear-animation" data-appear-animation="fadeInRightShorter">
+							<div class="col-md-12">
+								<h2 class="font-weight-normal text-6"><strong class="font-weight-extra-bold">And Then Advance To:</strong></h2>
+							</div>
+						</div>
+
+						<div class="row align-items-center pt-3 pb-3 appear-animation" data-appear-animation="fadeInRightShorter">
+							<div class="col-md-12 pr-md-5 mb-5 mb-md-0">
+								<h2 class="font-weight-normal text-6 mb-1"><strong class="font-weight-extra-bold">Gold</strong> Affiliate:</h2>
+								<p class="text-4">
+									When your personal group has a total of 108 Group eBook sales,
+									you will receive a Cash Bonus of $3,800
+								</p>
+							</div>
+						</div>
+
+						<div class="row align-items-center appear-animation" data-appear-animation="fadeInRightShorter">
+							<div class="col-md-12">
+								<h2 class="font-weight-normal text-6"><strong class="font-weight-extra-bold">And Then Advance To:</strong></h2>
+							</div>
+						</div>
+
+						<div class="row align-items-center pt-3 pb-3 appear-animation" data-appear-animation="fadeInRightShorter">
+							<div class="col-md-12 pr-md-5 mb-5 mb-md-0">
+								<h2 class="font-weight-normal text-6 mb-1"><strong class="font-weight-extra-bold">Ruby</strong> Affiliate:</h2>
+								<p class="text-4">
+									When your personal group has a total of 324 Group eBook sales,
+									you will receive a Cash Bonus of $22,000
+								</p>
+							</div>
+						</div>
+
+
+						<div class="row align-items-center appear-animation" data-appear-animation="fadeInRightShorter">
+							<div class="col-md-12">
+								<h2 class="font-weight-normal text-6"><strong class="font-weight-extra-bold">And Then Advance To:</strong></h2>
+							</div>
+						</div>
+
+						<div class="row align-items-center pt-3 pb-3 appear-animation" data-appear-animation="fadeInRightShorter">
+							<div class="col-md-12 pr-md-5 mb-5 mb-md-0">
+								<h2 class="font-weight-normal text-6 mb-1"><strong class="font-weight-extra-bold">DIAMOND</strong> Affiliate:</h2>
+								<p class="text-4">
+									When your personal group has a total of 972 Group eBook sales,
+									you will receive a Cash Bonus of over $400,000. This becomes even more exciting when
+									Diamond Affiliates in your personal group follow you, and the Diamond level repeats over and over again.
+
+								</p>
+							</div>
+						</div>
+
+
+
+					</div>
+				</div>
+			</div>
+		</section>
+		<div class="container pt-4">
+			<div class="row text-center pt-4 mt-5">
+				<div class="col">
+					<h2 class="word-rotator slide font-weight-bold text-8 mb-2">
+						<span> MCC’S Booster Co-op Option</span>
+						<br>
+						<span class="word-rotator-words bg-success">
+								<b class="is-visible">excited?</b>
+								<b>happy?</b>
+							</span>
+					</h2>
+					<h4 class="lead tall text-5">
+						When you elect to purchase an MCC Booster Pkg for $25 each (Limit of 20 pkgs), you will be instantly qualified for All MCC Cash Bonuses!
+					</h4>
 				</div>
 			</div>
 		</div>
-	</section> -->
 
-	<div class="container pb-1" style="padding-top: 8.5%;  font-family: Arial, Helvetica, sans-serif;">
+		<section class="section bg-color-grey section-height-3 border-0 mt-5 mb-0">
+			<div class="container">
+				<div class="row pt-4">
+					<div class="col">
+						<div class="overflow-hidden mb-3">
+							<h2 class="font-weight-bold text-8 mb-0 appear-animation" data-appear-animation="maskUp">
+								Each Booster Pkg Gives You Access to Our MCC’S Worldwide Co-op Marketing Campaign
+							</h2>
+							<p class="lead mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200">
+								The Campaign Goal is to Boost MCC’s Community by bringing over 100 eBook purchases Per Booster Pkg!
+							</p>
+						</div>
+					</div>
+				</div>
 
-		<div class="row pt-4 text-center">
-			<div class="col">
-				<div class="overflow-hidden mb-3">
-					<h2 class="font-weight-bold text-8 mb-0 appear-animation" data-appear-animation="maskUp">
-						How it Works
+				<div class="row align-items-center appear-animation" data-appear-animation="fadeInRightShorter">
+					<div class="col-md-12">
+						<h2 class="font-weight-normal text-6"><strong class="font-weight-extra-bold">PLUS</strong></h2>
+					</div>
+				</div>
+
+				<div class="row pt-4">
+					<div class="col">
+						<div class="overflow-hidden mb-3">
+							<p class="lead mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200">
+								As an Additional Cash Bonus, Affiliates can receive up to $15,000 Per Booster Pkg (Limit of 20 Pkgs) which can be purchased at any time.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="row align-items-center appear-animation" data-appear-animation="fadeInRightShorter">
+					<div class="col-md-12">
+						<h2 class="font-weight-normal text-6"><strong class="font-weight-extra-bold">As the Community Sales Grow…</strong></h2>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<div class="container pt-4">
+			<div class="row text-center pt-2 mt-2">
+				<div class="col">
+					<h2 class="word-rotator slide font-weight-bold text-8 mb-2">
+						<span> Free Bonus Products </span>
+						<br>
+						<span class="word-rotator-words bg-success">
+							<b class="is-visible">
+								Your Own Personal Self-Help eBook (22-yrs in the making)
+								“Awaken to Gentle Whispers”
+							</b>
+							<b>
+								And thereafter, Our Anti-Aging, Immune System Boosting Health Product
+								<br>
+								“Family Wellness Kit” (includes free S&H)
+							</b>
+						</span>
 					</h2>
 				</div>
 			</div>
-		</div>
 
-		<div class="row">
-			<div class="col-lg-12">
-				<div class="overflow-hidden">
-					<p class="lead mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200">
-						Welcome to The Majestic Cares Community!  “MCC” is an online community inspired by Ordinary People Worldwide. It was created so that ALL of its Affiliate/Members can receive a residual income for any worthwhile purpose, such as: business start-up, paying off debts, buying a new car or home, or travelling abroad to any desired destination!Welcome to The Majestic Cares Community!  “MCC” is an online community inspired by Ordinary People Worldwide. It was created so that ALL of its Affiliate/Members can receive a residual income for any worthwhile purpose, such as: business start-up, paying off debts, buying a new car or home, or travelling abroad to any desired destination!zz
-					<p class="lead mb-0 appear-animation mt-3" data-appear-animation="maskUp" data-appear-animation-delay="200">
-
-						No Matter what your current financial needs are, The Majestic Cares Online Community (MCC), is the answer to helping you and your love ones, to fulfill your financial hopes and dreams!
-					</p>
-
-					<p class="lead mb-0 appear-animation mt-3" data-appear-animation="maskUp" data-appear-animation-delay="200">
-						Please First Note, that this is NOT an MLM, Net-Work-Marketing, or a Get Rich
-						Quick Community. The MCC Online Community gives each Affiliate/Member, the unique opportunity of receiving Community Shared Cash Bonuses based on the
-						Total of Each Members Personal Group Sales Volume.
-
-					</p>
-
-					<p class="lead mb-0 appear-animation mt-3" data-appear-animation="maskUp" data-appear-animation-delay="200">
-						There are no Enrollment or Administration Fees. There is No Recruiting or Sponsoring Required to Become an MCC Sales Affiliate. However, in order to start receiving Community Shared Cash Bonuses, an Affiliate must make no less than 3 personal sales of MCC’S Self-Help Publication entitled, “Learn How to Stop Working for Money, and Let Your Money Work for You” which sells for $34.95, and comes with a 30-day Money Back Guarantee.
-					</p>
-
-					<h4 class="lead mb-0 mt-3"><strong>Here Is an Example of How This Works:</strong></h4>
-
-					<p class="lead mb-0 appear-animation mt-0" data-appear-animation="maskUp" data-appear-animation-delay="200">
-						When an MCC Sales Affiliate makes a minimum of 3 Personal Sales, the Affiliate qualifies to receive Community Shared Cash Bonuses and is positioned within an MCC Group of 12 other qualifiers who begin to receive Community Shared Cash Bonuses as per MCC’S Automated Compensation Plan.
-					</p>
-
-
-
-				</div>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="col-md-12 mt-5">
-				<h4><strong>Each Sales Affiliate Begins at the Bronze level:</strong></h4>
-			</div>
-		</div>
-			<div class="row">
-				<div class="col-md-12 text-center">
-					<h5> BRONZE Affiliate</h5>
-					<p class="text-dark text-5">
-						When your personal group has a total of 12 eBook sales,
-						you will receive a Cash Bonus of: $100
-					</p>
-
-					<h5>SILVER Affiliate</h5>
-					<p class="text-dark text-5">
-						When your personal group has total of 36 eBook sales,
-						you will receive a Cash Bonus of: $600
-					</p>
-
-					<h5>GOLD Affiliate</h5>
-					<p class="text-dark text-5 text-center">
-						When your personal group has total of 108 eBook sales,
-						you will receive a Cash Bonus of: $3,800
-					</p>
-
-					<h5>RUBY Affiliate</h5>
-					<p class="text-dark text-5 text-center">
-						<u>RUBY Affiliate</u>
-						<br>
-						When your personal group has total of 324 eBook sales,
-						you will receive a Cash Bonus of: $22,000
-					</p>
-
-					<h4>DIAMOND Affiliate</h4>
-					<p class="text-dark text-5 text-center">
-
-
-						<br>
-						When your personal group has a total of 972 eBook sales,
-						you will receive a Cash Bonus of over $400,000 - which repeats each time your
-						Diamond Group Affiliates has a total of 12 new eBook sales each!
-					</p>
-<!--					<p class="text-dark text-5">DIAMOND REPEAT: 12 eBook diamond group sales = $412,855.60  </p>-->
+			<div class="row text-center pt-1 mt-2">
+				<div class="col">
+					<h2 class="word-rotator slide font-weight-bold text-8 mb-2">
+						<span> And Much Much More! </span>
+					</h2>
 				</div>
 			</div>
 
-			<div class="row">
-				<div class="col-md-12 text-center mt-5">
-					<h4><strong>MCC’S Ad Booster Package Option
-					<p class="text-dark text-5">When you elect to purchase an MCC Booster Pkg for $25 each (Limit of 20 pkgs), you will be instantly qualified for All MCC Cash Bonuses!
-					</p>
-
-					<p class="text-dark text-5">Each Booster Package will enter you into MCC’S
-						Worldwide Cooperative Marketing Campaign
-						In Which You Will Receive Your 3 Qualifying Sales AND Boost
-						Your Total Group Sales With Over 100 Sales Per Package!
-					</p>
-					<p class="text-dark text-5">
-						Per Ad Booster Package (Limit of 20 Packages)
-					</p>
-						<h4><strong>MCC’S Ad Booster</strong></h4>
-					<p class="text-dark text-5">Your Own Personal Self-Help eBook (22-yrs in the making)
-						Entitled, “Awaken to Gentle Whispers”
-						And Our Anti-Aging, Immune System Boosting Health Product
-						“Shiaqga” 1- 2oz. & 1- 4oz. bottles (includes free S&H
-						And More!
-
-					</p>
+			<div class="row text-center pt-1 mt-2">
+				<div class="col">
+					<h2 class="word-rotator slide font-weight-bold text-8 mb-2">
+						<span> We look forward to seeing you on your continued journey to success! </span>
+					</h2>
 				</div>
 			</div>
-		<div class="row">
-				<p>As the Community Sales Grow…</p>
-		</div>
-		<div class="">
-		</div>
-	   </div>
-			<div class="row mt-3">
 
-			  <p class="lead mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200">
-				   CONGRATULATIONS ON BECOMING OUR NEXT MCC-er! YOU NOW QUALIFY TO RECEIVE UP-TO 90% PLUS OF YOUR TOTAL GROUP SALES VOLUME FOR LIFE!!
-			  </p>
+		</div>
 
+		<section class="call-to-action call-to-action-strong-grey content-align-center call-to-action-in-footer">
+			<div class="container py-5">
+				<div class="row py-3">
+					<div class="col-md-9 col-lg-9">
+						<div class="call-to-action-content">
+							<h2 class="font-weight-normal text-7 mb-0">Majestic Cares your <strong>Humanitarian</strong> Gateway</h2>
+						</div>
+					</div>
+					<div class="col-md-3 col-lg-3">
+						<div class="call-to-action-btn">
+							<a href="#" target="_blank" class="btn btn-dark btn-lg text-3 font-weight-semibold px-4 py-3 mt-5">Get Started Now</a><span class="arrow hlb d-none d-md-block" data-appear-animation="rotateInUpLeft" style="top: -90px; left: 70%;"></span>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="row">
-				<p class="lead mb-0 appear-animation mt-3" data-appear-animation="maskUp" data-appear-animation-delay="200">
-					<small>
-						<i>
-							Disclaimer: The MAJESTIC CARES COMMUNITY, OR MCC AND ITS AFFILIATES, is an online
-                            Sales Affiliate Community. This information,
-							as well as the information contained in our publications,
-							is for illustration purposes only, with no promises or guarantees of income.
-							Neither the administrators, subsidiaries, nor the
-							Affiliate/Members of the Majestic Cares Community, make no income guarantees.
-				        </i>
-				   </small>
-				</p>
-			</div>
+		</section>
+
 	</div>
-
 
 </template>
 <script>
