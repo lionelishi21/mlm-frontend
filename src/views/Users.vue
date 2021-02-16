b520d5fe4ba3dd712c9bb03f04353cb6cced8a42
Lionel Francis <lezardvaldth@gmail.com>
2021-02-16 16:39:21 -0500

    updates

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -57,30 +57,30 @@
             </div>
         </div>
         <!-- Modal -->
-<!--        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">-->
-<!--            <div class="modal-dialog" role="document">-->
-<!--                <div class="modal-content">-->
-<!--                    <div class="modal-header">-->
-<!--                        <h5 class="modal-title" id="exampleModalLabel">Fix Affiliate</h5>-->
-<!--                    </div>-->
-<!--                    <div class="modal-body">-->
-<!--                        <div class="form-row mb-4">-->
-<!--                            <div class="form-group col-md-12">-->
-<!--                                <label>Select the affiliate to give sales</label>-->
-<!--                                <select v-model="form.parent_id" class="form-control">-->
-<!--                                    <option v-for="usr in getAffiliates.response" :value="usr.affiliate_id">{{usr.name}}</option>-->
-<!--                                </select>-->
-<!--                            </div>-->
-<!--                        </div>-->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Fix Affiliate</h5>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-row mb-4">
+                            <div class="form-group col-md-12">
+                                <label>Select the affiliate to give sales</label>
+                                <select v-model="form.parent_id" class="form-control">
+                                    <option v-for="usr in getAffiliates.response" :value="usr.affiliate_id">{{usr.name}}</option>
+                                </select>
+                            </div>
+                        </div>
 
-<!--                    </div>-->
-<!--                    <div class="modal-footer">-->
-<!--                        <button class="btn" data-dismiss="modal"><i class="flaticon-cancel-12"></i> Discard</button>-->
-<!--                        <button @click="save()" type="button" class="btn btn-primary">Save</button>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn" data-dismiss="modal"><i class="flaticon-cancel-12"></i> Discard</button>
+                        <button @click="save()" type="button" class="btn btn-primary">Save</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="layout-px-spacing">
 
             <div class="row layout-top-spacing">
